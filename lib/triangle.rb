f82156a10d52a5316b740759338f20286e0b562f
Adam Clemons <adamlouisclemons@gmail.com>
2021-03-04 01:09:54 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+
+    sides = [@s1, @s2, @s3].sort
+
+    if sides[0] + sides[1] <= sides[2]
+      raise TriangleError
+    elsif sides.uniq.size == 1
+      type = :equilateral
+    elsif sides.uniq.size == 2
+      type = :isosceles
+    else
+      type = :scalene
+    end
+    type
+  end
+
+  class TriangleError < StandardError
+  end
+
+
 end
