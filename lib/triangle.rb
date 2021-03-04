8663a83e8a23aa255fbfb0e25347fa7626b38ebe
Adam Clemons <adamlouisclemons@gmail.com>
2021-03-04 01:02:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,20 @@
 class Triangle
-  # write code here
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+
+
+
+  end
+
+  class TriangleError < StandardError
+    
+  end
+
+
 end
